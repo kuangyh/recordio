63ea1bd367201f8b8d6b76ff6faa0db9500c4e2f
Yuheng Kuang <kuangyuheng@gmail.com>
2016-05-07 09:33:03 +0000

    Rewritten, optimize for minimize alloc and memory copy. Remove gzip support.

--- recordio.go
+++ recordio.go
@@ -1,208 +1,189 @@
-package recordio // import "github.com/kuangyh/recordio"
+package recordio
 
 import (
-	"bytes"
-	"compress/gzip"
 	"encoding/binary"
 	"errors"
 	"github.com/golang/snappy"
+	"hash"
 	"hash/crc32"
 	"io"
+	//"log"
 )
 
 var (
-	ErrReadBytes  = errors.New("Read bytes error")
-	ErrWriteBytes = errors.New("Write bytes error")
-	ErrChecksum   = errors.New("Checksum Error")
+	ReadBytesErr  = errors.New("Read bytes error")
+	WriteBytesErr = errors.New("Write bytes error")
+	ChecksumErr   = errors.New("Checksum Error")
 )
 
 const (
-	GzipCompress   = 1 << iota
-	SnappyCompress = 1 << iota
-	BodyChecksum   = 1 << iota
+	// By default RecordIO uses snappy to compress content, NoCompression disable
+	// this behavior on record level.
+	NoCompression = Flags(1 << iota)
 )
 
-const DefaultFlags = BodyChecksum | SnappyCompress
+const DefaultFlags = Flags(0)
 
 const (
-	recordHeaderSize = 16
+	recordHeaderStorageSize = 12 // bodyLength + flags + headerChecksum
 )
 
 type Flags uint32
 
-type RecordHeader struct {
-	BodyLength   uint32
-	Flags        Flags
-	BodyChecksum uint32
+type recordHeader struct {
+	bodyLength uint32
+	flags      Flags
 }
 
-func (header *RecordHeader) MarshalBinary() (data []byte, err error) {
-	output := &bytes.Buffer{}
-	output.Grow(recordHeaderSize)
-	if err := binary.Write(output, binary.LittleEndian, header); err != nil {
-		panic(err)
+func (header *recordHeader) encode(buf []byte) {
+	if len(buf) != 12 {
+		panic("recordHeader.encode with incorrect buffer size")
 	}
-	if err := binary.Write(output, binary.LittleEndian, crc32.ChecksumIEEE(output.Bytes())); err != nil {
-		panic(err)
-	}
-	return output.Bytes(), nil
+	binary.LittleEndian.PutUint32(buf[0:4], header.bodyLength)
+	binary.LittleEndian.PutUint32(buf[4:8], uint32(header.flags))
+	binary.LittleEndian.PutUint32(buf[8:12], crc32.ChecksumIEEE(buf[:8]))
 }
 
-func (header *RecordHeader) UnmarshalBinary(data []byte) error {
-	withChecksum := struct {
-		RecordHeader
-		Checksum uint32
-	}{}
-	r := bytes.NewReader(data)
-	if err := binary.Read(r, binary.LittleEndian, &withChecksum); err != nil {
-		panic(err)
+func (header *recordHeader) decode(buf []byte) error {
+	if len(buf) != 12 {
+		panic("recordHeader.decode with incorrect buffer size")
 	}
-	if withChecksum.Checksum != crc32.ChecksumIEEE(data[:12]) {
-		return ErrChecksum
+	headerChecksum := crc32.ChecksumIEEE(buf[:8])
+	if binary.LittleEndian.Uint32(buf[8:]) != headerChecksum {
+		return ChecksumErr
 	}
-	*header = withChecksum.RecordHeader
+	header.bodyLength = binary.LittleEndian.Uint32(buf[0:4])
+	header.flags = Flags(binary.LittleEndian.Uint32(buf[4:8]))
 	return nil
 }
 
-type Reader struct {
-	bytesReader      io.Reader
-	Options          Flags
-	BytesReaderError error
-	LastError        error
+type checksumWriter struct {
+	writer io.Writer
+	crc    hash.Hash32
 }
 
-func NewReader(reader io.Reader, options Flags) *Reader {
+func (cw *checksumWriter) Write(p []byte) (n int, err error) {
+	if n, err := cw.crc.Write(p); err != nil {
+		return n, err
+	}
+	return cw.writer.Write(p)
+}
+
+func (cw *checksumWriter) checksum() uint32 {
+	return cw.crc.Sum32()
+}
+
+type Reader struct {
+	// Last error
+	Err error
+
+	bytesReader   io.Reader
+	uncompressBuf []byte
+}
+
+// Create RecordIO reader from underlering io.Reader
+func NewReader(reader io.Reader) *Reader {
 	return &Reader{
-		bytesReader: reader,
-		Options:     options,
+		bytesReader:   reader,
+		uncompressBuf: make([]byte, 4096),
 	}
 }
 
-func (rr *Reader) err(err error, bytesReaderError error) error {
-	rr.LastError = err
-	rr.BytesReaderError = bytesReaderError
+func (rr *Reader) err(err error) error {
+	rr.Err = err
 	return err
 }
 
-func (rr *Reader) ReadRecord() ([]byte, error) {
-	if rr.LastError != nil {
-		return nil, rr.LastError
-	}
-	headerBytes := [recordHeaderSize]byte{}
-	if _, err := rr.bytesReader.Read(headerBytes[:]); err != nil {
-		if err == io.EOF {
-			return nil, rr.err(io.EOF, io.EOF)
-		} else {
-			return nil, rr.err(ErrReadBytes, err)
-		}
-	}
-	header := RecordHeader{}
-	if err := header.UnmarshalBinary(headerBytes[:]); err != nil {
-		return nil, rr.err(err, nil)
-	}
-	rawBytes := make([]byte, header.BodyLength)
-	if size, err := rr.bytesReader.Read(rawBytes); err != nil || uint32(size) != header.BodyLength {
-		return nil, rr.err(ErrReadBytes, err)
-	}
-
-	if rr.Options&BodyChecksum == BodyChecksum && header.Flags&BodyChecksum == BodyChecksum {
-		if header.BodyChecksum != crc32.ChecksumIEEE(rawBytes) {
-			return nil, rr.err(ErrChecksum, nil)
-		}
-	}
-
-	if header.Flags&GzipCompress == GzipCompress {
-		gzipReader, err := gzip.NewReader(bytes.NewReader(rawBytes))
-		if err != nil {
-			return nil, rr.err(ErrReadBytes, err)
-		}
-		defer gzipReader.Close()
-		buf := &bytes.Buffer{}
-		buf.Grow(int(header.BodyLength * 2))
-		_, err = io.Copy(buf, gzipReader)
-		if err != nil {
-			return nil, rr.err(ErrReadBytes, err)
-		}
-		return buf.Bytes(), nil
-	} else if header.Flags&SnappyCompress == SnappyCompress {
-		uncompressed, err := snappy.Decode(nil, rawBytes)
-		if err != nil {
-			return nil, rr.err(ErrReadBytes, err)
-		}
-		return uncompressed, nil
+func (rr *Reader) readBody(header recordHeader, dst []byte) (output []byte, err error) {
+	bodyLength := int(header.bodyLength)
+	if len(dst) < bodyLength+4 {
+		dst = make([]byte, bodyLength+4)
 	} else {
-		return rawBytes, nil
+		dst = dst[:bodyLength+4]
 	}
+	if size, _ := rr.bytesReader.Read(dst); size != len(dst) {
+		return nil, rr.err(ReadBytesErr)
+	}
+	if crc32.ChecksumIEEE(dst[:bodyLength]) != binary.LittleEndian.Uint32(dst[bodyLength:]) {
+		return nil, rr.err(ChecksumErr)
+	}
+	return dst[:bodyLength], nil
+}
+
+// Read next record. The returned slice may be a sub-slice of dst if dst was
+// large enough to hold the entire record. Otherwise, a newly allocated slice
+// will be returned. It's valid to pass nil dst
+func (rr *Reader) ReadRecord(dst []byte) (output []byte, err error) {
+	if rr.Err != nil {
+		return nil, rr.Err
+	}
+	headerBytes := [recordHeaderStorageSize]byte{}
+	if _, err = rr.bytesReader.Read(headerBytes[:]); err != nil {
+		return nil, rr.err(err)
+	}
+	header := recordHeader{}
+	if err = header.decode(headerBytes[:]); err != nil {
+		return nil, rr.err(err)
+	}
+	if header.flags&NoCompression != 0 {
+		return rr.readBody(header, dst)
+	}
+	rawBuf, err := rr.readBody(header, rr.uncompressBuf)
+	if err != nil {
+		return nil, err
+	}
+	buf, err := snappy.Decode(dst, rawBuf)
+	if err != nil {
+		return nil, rr.err(ReadBytesErr)
+	}
+	return buf, nil
 }
 
 type Writer struct {
-	bytesWriter      io.Writer
-	Options          Flags
-	BytesWriterError error
-	LastError        error
+	Err   error
+	Flags Flags
+
+	bytesWriter io.Writer
+	compressBuf []byte
 }
 
-func NewWriter(writer io.Writer, options Flags) *Writer {
+// Create RecordIO wrier that writes to underling io.Writer
+func NewWriter(writer io.Writer, flags Flags) *Writer {
 	return &Writer{
+		Flags:       flags,
 		bytesWriter: writer,
-		Options:     options,
+		compressBuf: make([]byte, 4096),
 	}
 }
 
-func (rw *Writer) err(err error, bytesWriterError error) error {
-	rw.LastError = err
-	rw.BytesWriterError = bytesWriterError
+func (rw *Writer) err(err error) error {
+	rw.Err = err
 	return err
 }
 
-func (rw *Writer) WriteRecord(data []byte, additionalFlags Flags) (size int, err error) {
-	if rw.LastError != nil {
-		return 0, rw.LastError
+// Write a record
+func (rw *Writer) WriteRecord(data []byte, flags Flags) error {
+	if rw.Err != nil {
+		return rw.Err
 	}
-	compressedData := data
-	flags := additionalFlags | rw.Options
-	if flags&GzipCompress == GzipCompress {
-		buf := bytes.NewBuffer(make([]byte, 0, len(data)))
-		gzipWriter := gzip.NewWriter(buf)
-		defer gzipWriter.Close()
-		if _, err := gzipWriter.Write(data); err != nil {
-			return 0, rw.err(ErrWriteBytes, err)
-		}
-		if err = gzipWriter.Close(); err != nil {
-			return 0, rw.err(ErrWriteBytes, err)
-		}
-		compressedData = buf.Bytes()
-	} else if flags&SnappyCompress == SnappyCompress {
-		compressedData = snappy.Encode(nil, data)
-	} else {
-		compressedData = data
+	flags = flags | rw.Flags
+	if flags&NoCompression == 0 {
+		data = snappy.Encode(rw.compressBuf, data)
 	}
-
-	header := RecordHeader{
-		BodyLength: uint32(len(compressedData)),
-		Flags:      flags,
+	header := recordHeader{bodyLength: uint32(len(data)), flags: flags}
+	var headerBuf [recordHeaderStorageSize]byte
+	header.encode(headerBuf[:])
+	if size, _ := rw.bytesWriter.Write(headerBuf[:]); size != recordHeaderStorageSize {
+		return rw.err(WriteBytesErr)
 	}
-	if flags&BodyChecksum == BodyChecksum {
-		header.BodyChecksum = crc32.ChecksumIEEE(compressedData)
+	bodyWriter := checksumWriter{writer: rw.bytesWriter, crc: crc32.NewIEEE()}
+	if size, _ := bodyWriter.Write(data); size != len(data) {
+		return rw.err(WriteBytesErr)
 	}
-	headerBin, err := header.MarshalBinary()
-	if err != nil {
-		return 0, rw.err(err, nil)
+	var checksumBuf [4]byte
+	binary.LittleEndian.PutUint32(checksumBuf[:], bodyWriter.checksum())
+	if size, _ := rw.bytesWriter.Write(checksumBuf[:]); size != len(checksumBuf) {
+		return rw.err(WriteBytesErr)
 	}
-
-	totalSize := 0
-	if size, err = rw.bytesWriter.Write(headerBin); size != len(headerBin) || err != nil {
-		return totalSize + size, rw.err(ErrWriteBytes, err)
-	}
-	totalSize += size
-	if size, err = rw.bytesWriter.Write(compressedData); size != len(compressedData) || err != nil {
-		return totalSize + size, rw.err(ErrWriteBytes, err)
-	}
-	totalSize += size
-	return totalSize, nil
-}
-
-// io.Writer
-func (rw *Writer) Write(data []byte) (n int, err error) {
-	return rw.WriteRecord(data, 0)
+	return nil
 }
