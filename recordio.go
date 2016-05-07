37da34adfcdeb36fc7511a77f3637c62428009f7
Yuheng Kuang <kuangyuheng@gmail.com>
2016-05-07 09:37:41 +0000

    update doc.

--- recordio.go
+++ recordio.go
@@ -1,9 +1,8 @@
-package recordio
-
 // Read/Write variable size record bytes to / from io.Reader / io.Writer, default
 // to use snappy compression.
 
 // Interface and implementation aims for minimize alloc and memory copy.
+package recordio
 
 import (
 	"encoding/binary"
